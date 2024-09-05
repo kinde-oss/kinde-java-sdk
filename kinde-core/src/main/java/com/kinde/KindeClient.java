9028585b4c813ac022e2fbc4aa9115575d37137b
Brett Chaldecott <brett@rapidhealth.co.uk>
2024-09-05 14:58:10 +0200

    SDK-REWORK Corrected comments

--- kinde-core/src/main/java/com/kinde/KindeClient.java
+++ kinde-core/src/main/java/com/kinde/KindeClient.java
@@ -11,7 +11,8 @@ public interface KindeClient {
      * This is a client session for a given access token provided by an end user.
      *
      * @param code The code from the server, used by the server to validate and generate a new
-     * @return
+     * @param authorizationUrl The url that was generated when the login request was made.
+     * @return A reference to the kinde client session for the code.
      */
     KindeClientSession initClientSession(String code, AuthorizationUrl authorizationUrl);
 
@@ -19,21 +20,21 @@ public interface KindeClient {
      * This is a client session for a given access token provided by an end user.
      *
      * @param kindeToken Either a refresh token used to generate a new token, or an access token used for role information
-     * @return
+     * @return A reference to the kinde client session for the given token.
      */
     KindeClientSession initClientSession(KindeToken kindeToken);
 
     /**
-     * Return an instance of the
-     * @return
+     * Return an instance of the client session for the service that instanciated this client. This is an M2M session.
+     * @return A session for the client. This is an M2M session or a session used to perform tasks against the API.
      */
     KindeClientSession clientSession();
 
 
     /**
-     * Return an instance of the token factory
+     * Return an instance of the token factory, used to validate tokens supplied to it.
      *
-     * @return
+     * @return A reference to the kinde token factory, used to validated and parse tokens.
      */
     KindeTokenFactory tokenFactory();
 
@@ -46,9 +47,9 @@ public interface KindeClient {
     KindeConfig kindeConfig();
 
     /**
-     * The configuration for this client.
+     * A reference to the OIDC meta data. This can be used to determine the configuration on the kinde server.
      *
-     * @return KindeConfig object
+     * @return An OIDC meta data object, retrieved from Kinde.
      */
     OidcMetaData oidcMetaData();
 }
