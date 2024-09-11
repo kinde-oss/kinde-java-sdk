b76e595099c8238a53a94410eeeb9fb599c76bf7
Brett Chaldecott <brett@rapidhealth.co.uk>
2024-09-11 11:41:06 +0200

    SDK-REWORK Update

--- playground/kinde-springboot-starter-example/src/main/java/com/kinde/spring/config/SecurityConfig.java
+++ playground/kinde-springboot-starter-example/src/main/java/com/kinde/spring/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.kinde.spring.config;
 
+import com.kinde.spring.resolver.CustomAuthorizationRequestResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,16 +15,25 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/home/**").authenticated()
-                        .requestMatchers("/**").permitAll()
-                )
-                .oauth2Client()
-                .and()
-                .oauth2Login()
-                .and()
-                .oauth2ResourceServer().jwt();
+    SecurityFilterChain securityFilterChain(HttpSecurity http, OAuth2AuthorizationRequestResolver customAuthorizationRequestResolver) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/","/index.css", "/registration", "/oauth2/authorization/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint
+                                .authorizationRequestResolver(customAuthorizationRequestResolver))
+                );
+
         return http.build();
+    }
+
+    // Redirect for /registration to trigger the registration flow
+    @Bean
+    public OAuth2AuthorizationRequestResolver customAuthorizationRequestResolver(ClientRegistrationRepository clientRegistrationRepository) {
+        DefaultOAuth2AuthorizationRequestResolver defaultResolver =
+                new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
+
+        return new CustomAuthorizationRequestResolver(defaultResolver);
     }
 }
