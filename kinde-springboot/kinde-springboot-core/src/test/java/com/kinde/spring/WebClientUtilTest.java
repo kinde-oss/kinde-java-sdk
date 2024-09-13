3e05525a80c08a7a44354774483644ab80a5156d
Brett Chaldecott <brett@rapidhealth.co.uk>
2024-09-13 13:21:56 +0200

    SDK-REWORK Working on coverage.

---
+++ kinde-springboot/kinde-springboot-core/src/test/java/com/kinde/spring/WebClientUtilTest.java
@@ -0,0 +1,51 @@
+package com.kinde.spring;
+
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.*;
+
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+
+@WireMockTest(httpPort = 8080)
+public class WebClientUtilTest {
+
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testWebClientHeaders() {
+        AtomicReference<HttpHeaders> headers = new AtomicReference<>();
+        WebClient client = WebClientUtil.createWebClient().mutate()
+                // mock out the response, we just want the headers
+                .exchangeFunction((request) -> {
+                        headers.set(request.headers());
+                        return Mono.just(ClientResponse.create(HttpStatus.OK).build());
+                    }
+                ).build();
+
+        client.get()
+                .uri("http://foo.example.com/")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println(headers.get().getFirst(HttpHeaders.USER_AGENT));
+    }
+}
