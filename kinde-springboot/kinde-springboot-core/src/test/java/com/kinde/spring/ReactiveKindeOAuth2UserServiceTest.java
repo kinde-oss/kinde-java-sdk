4b450cbe32ed3219b5fbe4f81bf6259adc18c30a
Brett Chaldecott <brett@rapidhealth.co.uk>
2024-09-25 12:30:42 +0200

    SDK-REWORK-TEST-COVERAGE Increased test coverage

---
+++ kinde-springboot/kinde-springboot-core/src/test/java/com/kinde/spring/ReactiveKindeOAuth2UserServiceTest.java
@@ -0,0 +1,31 @@
+package com.kinde.spring;
+
+import com.kinde.spring.sdk.KindeSdkClient;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+public class ReactiveKindeOAuth2UserServiceTest {
+
+    @Test
+    public void testLoadUser() {
+        ReactiveOAuth2UserService reactiveOAuth2UserService = Mockito.mock(ReactiveKindeOidcUserService.class);
+        KindeSdkClient kindeSdkClient = Mockito.mock(KindeSdkClient.class);
+        ReactiveKindeOidcUserService reactiveKindeOidcUserService = new ReactiveKindeOidcUserService(List.of(),reactiveOAuth2UserService,kindeSdkClient);
+
+        OidcUserRequest userRequest = Mockito.mock(OidcUserRequest.class);
+        ClientRegistration clientRegistration = Mockito.mock(ClientRegistration.class);
+        when(userRequest.getClientRegistration()).thenReturn(clientRegistration);
+        ClientRegistration.ProviderDetails providerDetails = Mockito.mock(ClientRegistration.ProviderDetails.class);
+        when(clientRegistration.getProviderDetails()).thenReturn(providerDetails);
+        ClientRegistration.ProviderDetails.UserInfoEndpoint userInfoEndpoint = Mockito.mock(ClientRegistration.ProviderDetails.UserInfoEndpoint.class);
+        when(providerDetails.getUserInfoEndpoint()).thenReturn(userInfoEndpoint);
+        reactiveKindeOidcUserService.loadUser(userRequest);
+    }
+}
