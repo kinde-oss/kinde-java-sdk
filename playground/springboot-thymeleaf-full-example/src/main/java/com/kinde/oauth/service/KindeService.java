14152372d3fa759d9592f6fa1b439f2a9c8810ca
Koman Rudden <komanrudden@gmail.com>
2025-05-15 18:02:10 +0200

    Issue-103 : removed repeated checks for kinde profile and added Kinde SDK logout

--- playground/springboot-thymeleaf-full-example/src/main/java/com/kinde/oauth/service/KindeService.java
+++ playground/springboot-thymeleaf-full-example/src/main/java/com/kinde/oauth/service/KindeService.java
@@ -1,13 +1,21 @@
 package com.kinde.oauth.service;
 
+import com.kinde.KindeClient;
+import com.kinde.KindeClientBuilder;
+import com.kinde.KindeTokenFactory;
 import com.kinde.oauth.model.KindeProfile;
+import com.kinde.token.KindeToken;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.core.AbstractOAuth2Token;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.HashSet;
@@ -20,6 +28,7 @@ import static org.springframework.security.oauth2.client.web.reactive.function.c
  * and retrieve user information.
  */
 @Service
+@Slf4j
 public class KindeService {
 
     private final OAuth2AuthorizedClientService authorizedClientService;
@@ -31,8 +40,7 @@ public class KindeService {
      * @param authorizedClientService the service for managing authorized OAuth2 clients.
      * @param userProfileClient       the {@link WebClient} used to fetch the user profile.
      */
-    public KindeService(OAuth2AuthorizedClientService authorizedClientService,
-                        WebClient userProfileClient) {
+    public KindeService(OAuth2AuthorizedClientService authorizedClientService, WebClient userProfileClient) {
         this.authorizedClientService = authorizedClientService;
         this.userProfileClient = userProfileClient;
     }
@@ -43,30 +51,47 @@ public class KindeService {
      *
      * @return a {@link KindeProfile} object containing user details.
      */
-    public KindeProfile loadDashboard() {
+    public String loadDashboard(HttpSession session, Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!authentication.isAuthenticated()) {
+            return "redirect:/login";
+        }
 
-        OAuth2AuthorizedClient authorizedClient = getOAuth2AuthorizedClient(SecurityContextHolder.getContext().getAuthentication());
+        KindeProfile kindeProfile = (KindeProfile) session.getAttribute("kindeProfile");
+        if (kindeProfile == null) {
+            DefaultOidcUser principal = (DefaultOidcUser) authentication.getPrincipal();
+            OAuth2AuthorizedClient authorizedClient = getOAuth2AuthorizedClient(authentication);
+            if (authorizedClient == null) {
+                log.error("Authorized client is null");
+                return "redirect:/login";
+            }
 
-        // Extracting user profile via Spring Webflux client
-        String userprofile = this.userProfileClient
-                .get()
-                .attributes(oauth2AuthorizedClient(authorizedClient))
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+            String userProfile = (String) session.getAttribute("userProfile");
+            if (userProfile == null) {
+                userProfile = this.userProfileClient.get().attributes(oauth2AuthorizedClient(authorizedClient)).retrieve().bodyToMono(String.class).block();
+                session.setAttribute("userProfile", userProfile);
+            }
 
-        DefaultOidcUser principal = (DefaultOidcUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String refreshToken = Optional.ofNullable(authorizedClient.getRefreshToken())
-            .map(AbstractOAuth2Token::getTokenValue)
-            .orElse(null);
-        return KindeProfile.builder()
-                .accessToken(authorizedClient.getAccessToken().getTokenValue())
-                .refreshToken(refreshToken)
-                .fullName(principal.getUserInfo().getFullName())
-                .idToken(principal.getIdToken().getTokenValue())
-                .roles(new HashSet<>(principal.getAuthorities()))
-                .userProfile(userprofile)
-                .build();
+            String refreshToken = Optional.ofNullable(authorizedClient.getRefreshToken()).map(AbstractOAuth2Token::getTokenValue).orElse(null);
+            kindeProfile = KindeProfile.builder()
+                    .accessToken(authorizedClient.getAccessToken().getTokenValue())
+                    .refreshToken(refreshToken)
+                    .fullName(Optional.ofNullable(principal.getUserInfo()).map(userInfo -> userInfo.getClaims().get("name")).map(Object::toString).filter(name -> !name.isEmpty()).orElse("Unknown"))
+                    .idToken(principal.getIdToken().getTokenValue())
+                    .roles(new HashSet<>(principal.getAuthorities()))
+                    .userProfile(userProfile)
+                    .build();
+            session.setAttribute("kindeProfile", kindeProfile);
+        }
+
+        boolean valid = validateToken();
+        if (!valid) {
+            log.error("Token is not valid");
+            return "redirect:/login";
+        }
+
+        model.addAttribute("kindeProfile", kindeProfile);
+        return "dashboard";
     }
 
     /**
@@ -78,5 +103,49 @@ public class KindeService {
     private OAuth2AuthorizedClient getOAuth2AuthorizedClient(Authentication authentication) {
         return authorizedClientService.loadAuthorizedClient("kinde", authentication.getName());
     }
-}
 
+    /**
+     * Validates the provided OAuth2 token using the Kinde SDK.
+     *
+     * @return true if the token is valid, false otherwise.
+     */
+    private boolean validateToken() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient("kinde", authentication.getName());
+        if (authorizedClient == null) {
+            log.error("Authorized client is null");
+            return false;
+        }
+
+        OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+        if (accessToken == null) {
+            log.error("Access token is null");
+            return false;
+        }
+
+        KindeClient kindeClient = KindeClientBuilder.builder().build();
+        KindeTokenFactory kindeTokenFactory = kindeClient.tokenFactory();
+        KindeToken parsedToken = kindeTokenFactory.parse(accessToken.getTokenValue());
+        if (!parsedToken.valid()) {
+            log.error("Parsed token is not valid");
+            return false;
+        }
+
+        return true;
+    }
+
+    public String callSdkApi(HttpSession session, Model model) {
+        KindeClient kindeClient = KindeClientBuilder.builder().build();
+        log.info("Kinde access token: {}", kindeClient.clientSession().retrieveTokens());
+        log.info("Kinde user info subject: {}", kindeClient.clientSession().retrieveUserInfo().getUserInfo().getSubject());
+        log.info("Kinde auth url: {}", kindeClient.clientSession().authorizationUrl().getUrl());
+
+        return "redirect:/dashboard";
+    }
+
+    public String logout() throws Exception {
+        KindeClient kindeClient = KindeClientBuilder.builder().build();
+        kindeClient.clientSession().logout();
+        return "redirect:/login";
+    }
+}
