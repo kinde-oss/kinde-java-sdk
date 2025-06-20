ede0c72e73a24d005cc74aa652568e3d8c9608f5
Koman Rudden <komanrudden@gmail.com>
2025-06-20 05:50:49 +0200

    fix: guava monorepo upgrade

--- kinde-core/src/main/java/com/kinde/session/KindeClientCodeSessionImpl.java
+++ kinde-core/src/main/java/com/kinde/session/KindeClientCodeSessionImpl.java
@@ -20,7 +20,7 @@ import com.nimbusds.openid.connect.sdk.UserInfoResponse;
 import com.nimbusds.openid.connect.sdk.token.OIDCTokens;
 import lombok.SneakyThrows;
 
-import javax.annotation.Nullable;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
